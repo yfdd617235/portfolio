802399d668c93ee9c0e13102c2866ab48cd6cee0
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2023-11-11 21:37:19 -0500

    create cols and scroll

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -4,10 +4,10 @@ import { Navbar, Nav } from 'react-bootstrap';
 const NavBar = ({ onNavItemSelect }) => {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Mi Portafolio</Navbar.Brand>
+      <Navbar.Brand href="#home">YOSEF DAVID GIRALDO SALAZAR</Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link onClick={() => onNavItemSelect('aboutMe')}>About Me</Nav.Link>
-        <Nav.Link onClick={() => onNavItemSelect('experience')}>Experience</Nav.Link>
+        <Nav.Link onClick={() => onNavItemSelect('aboutMe')}>About Me</Nav.Link> <br />
+        <Nav.Link onClick={() => onNavItemSelect('experience')}>Experience</Nav.Link> <br />
         <Nav.Link onClick={() => onNavItemSelect('projects')}>Projects</Nav.Link>
       </Nav>
     </Navbar>
--- src/components/navigator/Navigator.jsx
+++ src/components/navigator/Navigator.jsx
@@ -3,54 +3,54 @@ import NavBar from '../navBar/NavBar';
 import AboutMe from '../aboutme/AboutMe';
 import Projects from '../projects/Projects';
 import Experience from '../experience/Experience';
-
+import './navigator.css';
 
 const Navigator = () => {
-    const aboutMeRef = useRef(null);
-    const experienceRef = useRef(null);
-    const projectsRef = useRef(null);
+  const aboutMeRef = useRef(null);
+  const experienceRef = useRef(null);
+  const projectsRef = useRef(null);
 
-    const scrollToRef = (ref) => {
-        ref.current.scrollIntoView({ behavior: 'smooth' });
-    };
+  const scrollToRef = (ref) => {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  };
 
-    const handleNavItemSelect = (item) => {
-        switch (item) {
-            case 'aboutMe':
-                scrollToRef(aboutMeRef);
-                break;
-            case 'experience':
-                scrollToRef(experienceRef);
-                break;
-            case 'projects':
-                scrollToRef(projectsRef);
-                break;
-            default:
-                break;
-        }
-    };
+  const handleNavItemSelect = (item) => {
+    switch (item) {
+      case 'aboutMe':
+        scrollToRef(aboutMeRef);
+        break;
+      case 'experience':
+        scrollToRef(experienceRef);
+        break;
+      case 'projects':
+        scrollToRef(projectsRef);
+        break;
+      default:
+        break;
+    }
+  };
 
-    return (
-        <div className='screem'>
-            <div>
-                <NavBar onNavItemSelect={handleNavItemSelect} />
-            </div>
-
-            <div style={{ display: 'flex' }}>
-                <div style={{ flex: 1, overflowY: 'scroll', maxHeight: '100vh' }}>
-                    <div ref={aboutMeRef}>
-                        <AboutMe />
-                    </div>
-                    <div ref={experienceRef}>
-                        <Experience />
-                    </div>
-                    <div ref={projectsRef}>
-                        <Projects />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className='screem'>
+      <div className='left-col'>
+        <NavBar onNavItemSelect={handleNavItemSelect} />
+      </div>
+      <div className='right-col'>
+        <div className='right-content'>
+          <div ref={aboutMeRef}>
+            <AboutMe />
+          </div>
+          <div ref={experienceRef}>
+            <Experience />
+          </div>
+          <div ref={projectsRef}>
+            <Projects />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navigator;
+
