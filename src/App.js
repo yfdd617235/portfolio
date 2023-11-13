6fb9eca915a6db2e14b090bfc7284c1eb16f0428
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2023-11-12 23:59:25 -0500

    solve error App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navigator from './components/navigator/Navigator'
-import NavBar from './components/navBar/NavBar';
 
 function App() {
   return (
