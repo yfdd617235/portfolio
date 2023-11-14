6577c0e6d9250ad640f4b0cc32f45d39dbec1d8f
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2023-11-13 21:31:04 -0500

    update borders

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -5,7 +5,7 @@ function AboutMe() {
     return (
         <div className='aboutme'>
             <h4>About me</h4>
-            <p className='info-p'>I am a responsible and disciplined professional with over a decade of experience in engineering, excelling in areas such as planning, production control, personnel management, database administration, web development, and algorithm creation for trading. My ability to lead teams and projects, coupled with my technical expertise, has enabled me to contribute effectively in challenging environments. Passionate about continuous improvement, I am committed to bringing my diverse experience to innovative projects in the field of development and programming.</p>
+            <p className='info-r'>I am a responsible and disciplined professional with over a decade of experience in engineering, excelling in areas such as planning, production control, personnel management, database administration, web development, and algorithm creation for trading. My ability to lead teams and projects, coupled with my technical expertise, has enabled me to contribute effectively in challenging environments. Passionate about continuous improvement, I am committed to bringing my diverse experience to innovative projects in the field of development and programming.</p>
         </div>
 
     )
